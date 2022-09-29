f3efce4a4bacfc8c294544fc91bb8897612d3013
Nicholas Lexi <lexi.nicholas03@gmail.com>
2022-09-29 11:40:28 +0700

    commit

--- wishlist/views.py
+++ wishlist/views.py
@@ -18,7 +18,7 @@ def show_wishlist(request):
     data_wishlist_item = ItemWishlist.objects.all()
     context = {
     'list_item': data_wishlist_item,
-    'name': 'lexi',
+    'name': 'Nicholas Lexi',
     'last_login': request.COOKIES['last_login'],
 }
     return render(request, "wishlist.html", context)
